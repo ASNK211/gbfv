3925867731d791807fbdb262410df21920f5f585
ASNK211 <ASNK2AA@GMAIL.com>
2023-01-28 19:44:51 +0200

    first commit

---
+++ models/Product.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    article: {type: String, required: true},
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
